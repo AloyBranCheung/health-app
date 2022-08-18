4618a009d4598543574e27902d53c1fefe2912d9
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-18 13:58:04 -0400

    add featured, change homenavbar styling, change masthead styling

--- client/src/components/Homepage/Featured.tsx
+++ client/src/components/Homepage/Featured.tsx
@@ -1,5 +1,117 @@
 import React from "react";
 
 export default function Featured() {
-  return <div>Featured</div>;
+  return (
+    <section className="text-gray-400 bg-gray-900 body-font text-mainFontColor bg-mainColor">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-3xl text-4xl font-medium title-font mb-4 ">
+            convallis aenean et tortor
+          </h1>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices
+            eros in cursus turpis massa tincidunt dui ut.
+          </p>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-full md:w-1/2 p-4">
+            <div className="flex relative">
+              <a className="relative block bg-black group" href="">
+                <img
+                  className="absolute inset-0 object-cover w-full h-full transition-opacity opacity-75  group-hover:opacity-50"
+                  src="https://picsum.photos/1920/1080?random=1"
+                  alt=""
+                />
+                <div className="relative p-8">
+                  <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                    senectus
+                  </p>
+
+                  <p className="text-2xl font-bold text-white">
+                    tempor id eu nisl
+                  </p>
+
+                  <div className="mt-64">
+                    <div className="sm:transition-all sm:transform sm:translate-y-8 sm:opacity-0  sm:group-hover:opacity-100 sm:group-hover:translate-y-0">
+                      <p className="text-sm text-white">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Omnis perferendis hic asperiores quibusdam quidem
+                        voluptates doloremque reiciendis nostrum harum.
+                        Repudiandae?
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div className="lg:w-1/2 md:w-1/2 p-4">
+            <div className="flex relative">
+              <a className="relative block bg-black group" href="">
+                <img
+                  className="absolute inset-0 object-cover w-full h-full transition-opacity opacity-75  group-hover:opacity-50"
+                  src="https://picsum.photos/1920/1080?random=2"
+                  alt=""
+                />
+                <div className="relative p-8">
+                  <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                    senectus
+                  </p>
+
+                  <p className="text-2xl font-bold text-white">
+                    tempor id eu nisl
+                  </p>
+
+                  <div className="mt-64">
+                    <div className="sm:transition-all sm:transform sm:translate-y-8 sm:opacity-0  sm:group-hover:opacity-100 sm:group-hover:translate-y-0">
+                      <p className="text-sm text-white">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Omnis perferendis hic asperiores quibusdam quidem
+                        voluptates doloremque reiciendis nostrum harum.
+                        Repudiandae?
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div className="lg:w-1/2 md:w-full p-4">
+            <div className="flex relative">
+              <a className="relative block bg-black group" href="">
+                <img
+                  className="absolute inset-0 object-cover w-full h-full transition-opacity opacity-75  group-hover:opacity-50"
+                  src="https://picsum.photos/1920/1080?random=3"
+                  alt=""
+                />
+                <div className="relative p-8">
+                  <p className="text-sm font-medium tracking-widest text-pink-500 uppercase">
+                    senectus
+                  </p>
+
+                  <p className="text-2xl font-bold text-white">
+                    tempor id eu nisl
+                  </p>
+
+                  <div className="mt-64">
+                    <div className="sm:transition-all sm:transform sm:translate-y-8 sm:opacity-0  sm:group-hover:opacity-100 sm:group-hover:translate-y-0">
+                      <p className="text-sm text-white">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Omnis perferendis hic asperiores quibusdam quidem
+                        voluptates doloremque reiciendis nostrum harum.
+                        Repudiandae?
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- client/src/components/Homepage/HomeNavbar.tsx
+++ client/src/components/Homepage/HomeNavbar.tsx
@@ -63,24 +63,34 @@ export default function HomeNavbar() {
   );
 
   return (
-    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4 absolute z-10">
-      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4 absolute z-10 border-none">
+      <div className="container mx-auto flex items-center justify-between text-white">
         <Typography
           as="a"
           href="#"
           variant="small"
           className="mr-4 cursor-pointer py-1.5 font-normal"
         >
-          <strong>Random Health Tech</strong>
+          <strong className="text-xl">Random Health Tech</strong>
         </Typography>
         <div className="hidden lg:block">{navList}</div>
-        <Button
-          variant="gradient"
-          size="sm"
-          className="hidden lg:inline-block bg-tertiaryColor text-mainFontColor"
-        >
-          <span>Login/Sign Up</span>
-        </Button>
+        <div className="flex flex-row gap-4">
+          <Button
+            variant="gradient"
+            size="sm"
+            className="hidden lg:inline-block bg-tertiaryColor text-mainFontColor"
+          >
+            <span className="text-sm">Login</span>
+          </Button>
+          <Button
+            variant="gradient"
+            size="sm"
+            className="hidden lg:inline-block bg-mainColor text-mainFontColor"
+          >
+            <span className="text-sm">Sign Up</span>
+          </Button>
+        </div>
+
         <IconButton
           variant="text"
           className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden [&>span]:relative"
@@ -120,19 +130,30 @@ export default function HomeNavbar() {
         </IconButton>
       </div>
       <MobileNav
-        className="bg-mainColor p-5 rounded-lg mt-3 text-mainFontColor"
+        className="bg-mainColor p-5 rounded-lg mt-3 text-mainFontColor flex flex-col justify-between !h-full"
         open={openNav}
       >
-        {navList}
-        <Button
-          ripple={true}
-          variant="gradient"
-          size="sm"
-          fullWidth
-          className="bg-tertiaryColor mb-5 text-mainFontColor"
-        >
-          <span>Buy Now</span>
-        </Button>
+        <div>{navList}</div>
+        <div className="flex flex-row gap-5">
+          <Button
+            ripple={true}
+            variant="gradient"
+            size="sm"
+            fullWidth
+            className="bg-mainColor text-mainFontColor shadow"
+          >
+            <span>Login</span>
+          </Button>
+          <Button
+            ripple={true}
+            variant="gradient"
+            size="sm"
+            fullWidth
+            className="bg-tertiaryColor text-mainFontColor"
+          >
+            <span>Sign Up</span>
+          </Button>
+        </div>
       </MobileNav>
     </Navbar>
   );
