b26e2cbc097abf532f50c00f71aef8c52b0cd065
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-18 16:29:09 -0400

    add routes to login, contact, careers and about us

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // components
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ContactUs from "./pages/ContactUs";
+import Careers from "./pages/Careers";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   );
