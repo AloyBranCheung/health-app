c4bf5ee713f789c4f8d6dabd75cee27ff1953f7c
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-18 10:13:42 -0400

    add tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "400px",
+      md: "768px",
+      lg: "1200px",
+    },
+    colors: {
+      mainColor: "#F6FFFF",
+      secondaryColor: "#FFE7E7",
+      tertiaryColor: "#FDDD6D",
+      mainFontColor: "#423066",
+      white: "white",
+      test: "red",
+    },
     extend: {},
   },
   plugins: [],
