f31022fb15d83b56d2a20e1f39c776befd6642fe
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-18 10:14:01 -0400

    add routes, add tailwind config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// components
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
