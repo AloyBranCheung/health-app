b4dd94d4a6c13633dd5ed52a4dddf22e7ba59547
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-19 16:38:36 -0400

    add user/register route and add user model

---
+++ api/models/User.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    MRN: {
+      type: String,
+    },
+    familyMembers: {
+      type: [String],
+    },
+    isProvider: {
+      type: Boolean,
+      default: false,
+    },
+    patientList: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Users", UserSchema);
