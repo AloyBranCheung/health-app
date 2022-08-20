641cb61ea0361c81aa7f947b07845468cb78d5c9
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-08-19 20:57:32 -0400

    add logo and styling

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -15,7 +15,10 @@ export default function Login() {
       exit={{ opacity: 0 }}
     >
       <section className="text-mainFontColor bg-mainColor h-screen flex flex-row relative">
-        <div className="hidden md:flex bg-secondaryColor w-1/2 justify-center items-center">
+        <div className="hidden md:flex md:flex-col bg-secondaryColor w-1/2 justify-center items-center md:gap-10 md:p-10">
+          <h1 className="text-5xl text-mainFontColor font-bold">
+            Start your journey here.
+          </h1>
           <div>
             <img
               src="https://cdn-icons-png.flaticon.com/512/8288/8288882.png"
@@ -23,8 +26,13 @@ export default function Login() {
             />
           </div>
         </div>
-        <div className="flex flex-col justify-between items-center w-full md:w-1/2 p-3 md:py-16 md:px-5 ">
-          <div className="p-5 w-full max-w-sm">
+        <div className="flex flex-col justify-center items-center w-full md:w-1/2 p-3 md:py-16 md:px-5 ">
+          <div className="flex p-5 w-full max-w-sm">
+            <img
+              className="h-7 w-7 self-center mr-3"
+              src="https://cdn-icons.flaticon.com/png/512/1177/premium/1177232.png?token=exp=1660956265~hmac=d965bf48482fcf14909c113d38aeb5dc"
+              alt="brandimg"
+            />
             <h1>
               <strong className="text-2xl cursor-pointer" onClick={navHome}>
                 Random Health Tech
