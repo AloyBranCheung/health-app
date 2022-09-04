c6da9b1e609bc93644d841eee12b7e9f2a979d69
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-09-04 10:49:00 -0400

    test

--- client/src/components/HealthDashboard/HealthHistory/DiseasesList.tsx
+++ client/src/components/HealthDashboard/HealthHistory/DiseasesList.tsx
@@ -6,7 +6,9 @@ type Props = {
 };
 
 export default function DiseasesList({ className, diseases }: Props) {
-  const listDiseases = diseases.map((disease) => <li>{disease}</li>);
+  const listDiseases = diseases.map((disease, index) => (
+    <li key={index}>{disease}</li>
+  ));
   return (
     <ul className={`grid md:grid-cols-2 gap-x-5 w-full ${className}`}>
       {listDiseases}
