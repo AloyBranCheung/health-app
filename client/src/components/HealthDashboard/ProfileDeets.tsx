8f0291862118d098ff552b32c419ea804c0cd233
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-09-11 22:13:55 -0400

    abstract profile details

---
+++ client/src/components/HealthDashboard/ProfileDeets.tsx
@@ -0,0 +1,60 @@
+import React, { useContext } from "react";
+import AuthContext from "../../context/authContext";
+
+export default function ProfileDeets() {
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="flex flex-col w-full gap-2 justify-center md:justify-around">
+      <div className="flex flex-row gap-2 ">
+        <div className="flex w-full items-center justify-center basis-1/12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <h1 className="w-full">{user?.phoneNumber}</h1>
+      </div>
+
+      <div className="flex flex-row gap-2 ">
+        <div className="flex w-full items-center justify-center basis-1/12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6.912 3a3 3 0 00-2.868 2.118l-2.411 7.838a3 3 0 00-.133.882V18a3 3 0 003 3h15a3 3 0 003-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0017.088 3H6.912zm13.823 9.75l-2.213-7.191A1.5 1.5 0 0017.088 4.5H6.912a1.5 1.5 0 00-1.434 1.059L3.265 12.75H6.11a3 3 0 012.684 1.658l.256.513a1.5 1.5 0 001.342.829h3.218a1.5 1.5 0 001.342-.83l.256-.512a3 3 0 012.684-1.658h2.844z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <h1 className="w-full">{user?.email}</h1>
+      </div>
+
+      <div className="flex flex-row gap-2 ">
+        <div className="flex w-full items-center justify-center basis-1/12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+            <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+          </svg>
+        </div>
+        <h1 className="w-full">{user?.address}</h1>
+      </div>
+    </div>
+  );
+}
