87dfca8a0ff7efbf722525b8eb44b6819c22c8ab
Brandon <aloysiusbrandon.cheung@mail.utoronto.ca>
2022-09-16 22:57:56 +0000

    remove unused variable

--- client/src/components/EditProfile/EditProfileForm.tsx
+++ client/src/components/EditProfile/EditProfileForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import AuthContext from "../../context/authContext";
 import AddressInput from "./AddressInput";
 import ButtonContainer from "./ButtonContainer";
