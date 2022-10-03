e2ddcad66831e280d4da1e12f737ed6854edcae0
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-10-03 19:38:32 -0400

    start viewfamilyprofile

---
+++ client/src/components/HealthDashboard/MainCard/ViewFamilyProfile/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function ViewFamilyProfile() {
+  // need to create a modal and also link it to "edit" button in "My Family"
+  return <div>ViewFamilyProfile</div>;
+}
