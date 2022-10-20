3d0d6d00a06dafc58ff684320d3a463889f86cfe
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-10-20 09:55:08 -0400

    start recent labs

---
+++ client/src/components/HealthDashboard/MainCard/ViewFamilyProfile/DisplayInformation/RecentLabwork/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function RecentLabwork() {
+  return (
+    <div>
+      <h1 className="text-lg">
+        <strong>Recent Labwork</strong>
+      </h1>
+    </div>
+  );
+}
