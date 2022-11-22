e5e9e78d136b13942392f501b3c15c46b13df8b2
Brandon <brandon.cheung.dev@gmail.com>
2022-11-22 18:27:17 -0500

    Revert "adds view available profile pics and change your profile picture feature "

--- client/src/components/HealthDashboard/MyProfile.tsx
+++ client/src/components/HealthDashboard/MyProfile.tsx
@@ -6,7 +6,6 @@ import LoadingSpinner from "../UI/LoadingSpinner";
 import ProfileDeets from "./ProfileDeets";
 import ModalContext from "../../context/modalContext";
 import EditProfile from "../EditProfile/EditProfile";
-import ViewProfilePics from "../ViewProfilePics";
 
 type Props = {
   className: string;
@@ -20,10 +19,6 @@ export default function MyProfile({ className }: Props) {
     onVisible("editProfile");
   };
 
-  const handleViewProfilePics = () => {
-    onVisible("viewProfilePics");
-  };
-
   return (
     <Card className={className}>
       {isLoading ? (
@@ -31,7 +26,6 @@ export default function MyProfile({ className }: Props) {
       ) : (
         <>
           {isVisible === "editProfile" && <EditProfile />}
-          {isVisible === "viewProfilePics" && <ViewProfilePics />}
           <div className="p-5 flex flex-col gap-5 w-full md:h-full">
             <div className="flex flex-row items-center justify-between w-full">
               <h1>
@@ -41,17 +35,11 @@ export default function MyProfile({ className }: Props) {
             </div>
             <div className="flex flex-col w-full h-full items-center md:flex-row gap-2 md:overflow-y-scroll">
               <ProfileDeets />
-              <div
-                onClick={handleViewProfilePics}
-                className="w-full flex items-center justify-center rounded-full relative"
-              >
-                <span className="text-transparent hover:text-white cursor-pointer z-50 absolute w-full h-full flex items-center justify-center hover:bg-modalBackground hover:ease-in ease-out hover:duration-200 rounded-full">
-                  Change Profile Pics
-                </span>
+              <div className="w-full flex items-center justify-center">
                 <img
                   src={user.profilePic}
                   alt="profileClipboard"
-                  className="hover:cursor-pointer"
+                  className=""
                 />
               </div>
             </div>
